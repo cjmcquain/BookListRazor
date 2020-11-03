69aadc29d091ab401f02be143aea02e221584a56
Charles McQuain <charlesmcquain@gmail.com>
2020-11-02 21:53:26 -0800

    Added form to Create New Book page, added ISBN to Book model, ran migration

--- Pages/BookList/Create.cshtml.cs
+++ Pages/BookList/Create.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookListRazor.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +10,18 @@ namespace BookListRazor.Pages.BookList
 {
     public class CreateModel : PageModel
     {
+        private readonly ApplicationDbContext _db;
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public Book Book { get; set; }
+
         public void OnGet()
         {
+
         }
     }
 }
